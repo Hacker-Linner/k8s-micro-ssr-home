7a1e26339eae58c6c04ba0df36b0e09a8ad9f10b
Kirk-Wang <kirk.w.wang@gmail.com>
2020-12-03 18:11:31 +0800

    update

--- ssr-home/src/views/home.vue
+++ ssr-home/src/views/home.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <p v-if="$route.query.tab !== 'new'">
-            我是首页 ——
-            <a href="https://k8seggjs.hacker-linner.com/" target="_blank"
-                >互相交流，一起云原生！</a
-            >
-        </p>
+        <div v-if="$route.query.tab !== 'new'">
+            <h3>我是首页渲染微服务 —— <a href="https://github.com/Hacker-Linner/k8s-micro-ssr-home" target="_blank">k8s-micro-ssr-home</a></h3>
+             <div>
+                <img src="https://pic2.zhimg.com/v2-7029d91276e5fb38b003606c0ebbae2b_1440w.jpg?source=172ae18b" width="65%"/>
+            </div>
+        </div>
         <p v-else>首页最新内容</p>
     </div>
 </template>
