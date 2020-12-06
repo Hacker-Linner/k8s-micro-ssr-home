e3654bb9df14df8872928f70c94282e427f26b53
Kirk-Wang <kirk.w.wang@gmail.com>
2020-12-06 23:09:00 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -8,12 +8,12 @@ COPY yarn.lock /app/dependencies/yarn.lock
 RUN cd /app/dependencies \
     && yarn install --frozen-lockfile --registry=https://registry.npm.taobao.org \
     && yarn cache clean \
-    && mkdir /app/egg \
-    && ln -s /app/dependencies/node_modules /app/egg/node_modules
+    && mkdir /app/micro-ssr \
+    && ln -s /app/dependencies/node_modules /app/micro-ssr/node_modules
 
-COPY ./ /app/egg/
+COPY ./ /app/micro-ssr/
 
-WORKDIR /app/egg
+WORKDIR /app/micro-ssr
 EXPOSE 7001
 
 RUN yarn build
--- ssr-home/src/views/home.vue
+++ ssr-home/src/views/home.vue
@@ -2,8 +2,22 @@
     <div>
         <div v-if="$route.query.tab !== 'new'">
             <h3>我是首页渲染微服务 —— <a href="https://github.com/Hacker-Linner/micro-ssr-home" target="_blank">micro-ssr-home</a></h3>
-             <div>
+            <div>
                 <img src="https://pic2.zhimg.com/v2-7029d91276e5fb38b003606c0ebbae2b_1440w.jpg?source=172ae18b" width="65%"/>
+            </div>
+            <div style="display:flex;">
+                <div>
+                    <p>前端云原生群</p>
+                    <img src="https://k8seggjs.hacker-linner.com/public/images/team-qrcode.jpeg" width="200px"/>
+                </div>
+                <div>
+                    <p>公众号</p>
+                    <img src="https://k8seggjs.hacker-linner.com/public/images/tea-qrcode.png" width="200px"/>
+                </div>
+                <div>
+                    <p>我的微信</p>
+                    <img src="https://k8seggjs.hacker-linner.com/public/images/me-qrcode.jpeg" width="200px"/>
+                </div>
             </div>
         </div>
         <p v-else>首页最新内容</p>
